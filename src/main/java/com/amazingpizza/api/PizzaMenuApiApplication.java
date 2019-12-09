c5fc75ac6b90e1d577d8929e97b3467561aa4e01
Alvaro Zambrana <alvarozs.js@gmail.com>
2019-12-09 00:58:42 -0400

    PM-009 add global CORS configuration

--- src/main/java/com/amazingpizza/api/PizzaMenuApiApplication.java
+++ src/main/java/com/amazingpizza/api/PizzaMenuApiApplication.java
@@ -2,9 +2,22 @@ package com.amazingpizza.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
 public class PizzaMenuApiApplication {
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurerAdapter() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**");
+			}
+		};
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(PizzaMenuApiApplication.class, args);
